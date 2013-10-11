efeb31a4f47aea3a588c8f23cbcad04a7b37ad35
Lorenzo <lorenzo.bossi@uninsubria.it>
2013-10-12 01:47:45 +0200

    heavy refatoring

--- js/picstomap.js
+++ js/picstomap.js
@@ -9,7 +9,6 @@ var GeotaggedPicsToMap = {
 
 jQuery(document).ready(function( $ ) {
 
-
     var Barrier = function(triggerAfter, finishFunction, progressFunction){
 	var This = this;
 	this._callbackFunctionsNum = triggerAfter;
@@ -33,28 +32,6 @@ jQuery(document).ready(function( $ ) {
 	}
     }
     
-    var binaryDownload = function(url, callback) {
-	var req = new XMLHttpRequest();
-	req.open("GET", url, true);
-	req.responseType = "arraybuffer";
-	req.onload = function (event) {
-	    callback(req.response);
-	};
-	req.onerror = function () {
-	    callback(null);
-	}
-	req.send(null);
-    }
-
-    var readExif = function(url, callback) {
-	binaryDownload(url, function(data){
-            var exif = new ExifReader();
-            exif.load(data);
-            var tags = exif.getAllTags();
-            callback(tags);
-	});
-    }
-
     var ImageFrame = function(src) {
 	var This = this;
 	this.src = src;
@@ -110,94 +87,57 @@ jQuery(document).ready(function( $ ) {
 	return this;
     }
     
-
-    var PicsToMap = function() {
+    var Marker = function(latitude, longitude, image, thumbnail) {
 	var This = this;
-	var points = [];
+	
+	this.GPSLongitude = longitude;
+	this.GPSLatitude = latitude; 
+	this.img = image;
+	this.thumbnail = thumbnail;
+
+	this._imgFrame = new ImageFrame(this.img);
+	
+	this.openWindow = function() {
+	    This._imgFrame.open();
+	}
+    }
+
+    var MapWindow = function() {
 	var mapDialog;
 	var mapHolder;
-	var map;
 	var progressBar;
-	this._waitingForPoints = null;
 
-	var addImageClickListener = function(marker, src) {
-	    var img = new ImageFrame(src);
-	    google.maps.event.addListener(marker, 'click', img.open);
+	this.hideProgressBar = function() {
+	    progressBar.hide();
+	}
+	this.getMapContainer = function() {
+	    return mapHolder.get(0);
 	}
 	
-	var loadGoogleMaps = function() {
-	    progressBar.hide();
-	    mapDialog.dialog('option', 'title', GeotaggedPicsToMap.dialogTitle.replace('0', points.length));
-	    if ( points.length == 0 ) {
-		mapDialog.text(GeotaggedPicsToMap.sorry);
-		return;
-	    }
-	    
-            map = new google.maps.Map(mapHolder.get(0), {
-		center: new google.maps.LatLng(points[0].GPSLatitude, points[0].GPSLongitude ),
-		zoom: 7,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-            });
-	    
-	    var bounds = new google.maps.LatLngBounds ();	
-	    for (var n = 0 ; n < points.length ; n++){
-		var marker = new google.maps.Marker({
-		    position: new google.maps.LatLng(points[n].GPSLatitude, points[n].GPSLongitude),
-		    map: map,
-		    icon: {
-		    	url : points[n].thumbnail,
-		    	scaledSize : new google.maps.Size(45,45)
-		    }
-		});
-		console.log(points[n])
-		bounds.extend(marker.position);
-		addImageClickListener(marker, points[n].img);
-	    }
-	    map.fitBounds(bounds);
-	}
-
-	var prepareData = function(img, finish, progress){
-	    if ( This._waitingForPoints !== null)
-		return;
-	    This._waitingForPoints = new Barrier(img.length, finish, progress);
-	    img.each(
-		function(index, imgUrl) { 
-		    readExif(this.image, This._waitingForPoints.createCallback(function(tags) {
-			if(tags !== null && tags['GPSLongitude'] && tags['GPSLatitude']){
-			    var point = {
-				GPSLongitude : tags['GPSLongitude'].description,
-				GPSLatitude : tags['GPSLatitude'].description,
-				img : imgUrl.image,
-				thumbnail :  imgUrl.thumbnail
-			    };
-			    points.push(point);
-			}
-		    }));
-		}
-	    );
-	}
-
-	var openMap = function(){
-	    var img = $('a[href$=".jpg"]:has(img)').map(function(idx) {
-		return {
-		    image : this.href,
-		    thumbnail : $(this).find('img').attr('src')
-		}
-	    });
-	    
-	    progressBar.progressbar( { max: img.length, disabled: true } );
+	this.open = function() {
 	    mapDialog.dialog('open');
-	    prepareData(img, 
-			loadGoogleMaps, 
-			function(curr, max){ progressBar.progressbar('option', 'disabled', false); progressBar.progressbar('value', curr); } 
-		       );
-	    return false;
 	}
-
+	
+	this.close = function() {
+	    mapDialog.dialog('close');
+	}
+	
+	this.progress = function(curr, max) {
+	    progressBar.progressbar('option', 'disabled', false); 
+	    progressBar.progressbar('option', 'max', max);
+	    progressBar.progressbar('value', curr);
+	    mapDialog.dialog('option', 'title', GeotaggedPicsToMap.dialogTitle.replace('0', curr));
+	}
+	
+	this.failToLoad = function() {
+	    mapDialog.text(GeotaggedPicsToMap.sorry);
+	}
 
 	//ctor
 	mapDialog = $('<div class="picstomap"></div>');
 	progressBar = $('<div class="picstomapPB" style="left: 2em; position: absolute; right: 2em; top: 2em;"/>');
+	progressBar.progressbar( { disabled: true } );
+
 	mapHolder = $('<div class="picstomapMH" style="height: 100%; width: 100%;"/>');
 	mapDialog.append(mapHolder);
 	mapDialog.append(progressBar);
@@ -211,7 +151,7 @@ jQuery(document).ready(function( $ ) {
 	    width: $(window).width() * 0.8,
 	    height: $(window).height() * 0.8
 	});
-
+	
 	$(window).resize(function() {
 	    mapDialog.dialog("option", "width", $(window).width() * 0.8);
 	    mapDialog.dialog("option", "height", $(window).height() * 0.8);
@@ -219,10 +159,126 @@ jQuery(document).ready(function( $ ) {
 	jQuery(window).scroll(function() {
 	    mapDialog.dialog('option','position','center');    
 	});
-	
-	$('.geotagged_pics_open_map').click(openMap)
     }
     
-    new PicsToMap()
+    var PicsAnalyzer = function() {
+	var This = this;
+	this._waitingForPoints = null;
+	this.markers = [];
+	
+	this.binaryDownload = function(url, callback) {
+	    var req = new XMLHttpRequest();
+	    req.open("GET", url, true);
+	    req.responseType = "arraybuffer";
+	    req.onload = function (event) {
+		callback(req.response);
+	    };
+	    req.onerror = function () {
+		callback(null);
+	    }
+	    req.send(null);
+	}
+	
+	this.readExif = function(url, callback) {
+	    This.binaryDownload(url, function(data){
+		var exif = new ExifReader();
+		exif.load(data);
+		var tags = exif.getAllTags();
+		callback(tags);
+	    });
+	}
+	
+	this.bind = function() {
+	    $(this).bind.apply($(this), arguments);
+	}
+	
+	
+	this.loadImages = function(){
+	    var img = $('a[href$=".jpg"]:has(img)').map(function(idx) {
+		return {
+		    image : this.href,
+		    thumbnail : $(this).find('img').attr('src')
+		}
+	    });
+
+	    This._waitingForPoints = new Barrier(img.length,
+						 function() { $(This).trigger('finish', [This.markers] ); } ,
+						 function(curr, max) { $(This).trigger('progress', [curr, max] ); } );
+
+	    img.each(
+		function(index, imgUrl) { 
+		    This.readExif(this.image, This._waitingForPoints.createCallback(function(tags) {
+			if(tags !== null && tags['GPSLongitude'] && tags['GPSLatitude']){
+			    var point = new Marker(tags['GPSLatitude'].description, tags['GPSLongitude'].description, imgUrl.image, imgUrl.thumbnail);
+			    This.markers.push(point);
+			}
+		    }));
+		}
+	    );
+	}
+    }
     
+    var GoogleMaps = function() {
+	var map;
+
+	this.loadMap = function(mapHolderDiv, markers) {
+            map = new google.maps.Map(mapHolderDiv, {
+		center: new google.maps.LatLng(markers[0].GPSLatitude, markers[0].GPSLongitude ),
+		zoom: 7,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+	    
+	    var bounds = new google.maps.LatLngBounds ();	
+	    for (var n = 0 ; n < markers.length ; n++){
+		var marker = new google.maps.Marker({
+		    position: new google.maps.LatLng(markers[n].GPSLatitude, markers[n].GPSLongitude),
+		    map: map,
+		    icon: {
+		    	url : markers[n].thumbnail,
+		    	scaledSize : new google.maps.Size(45,45)
+		    }
+		});
+		bounds.extend(marker.position);
+		
+		google.maps.event.addListener(marker, 'click', markers[n].openWindow);
+	    }
+	    map.fitBounds(bounds);
+	}
+    }
+    
+    var PicsToMap = function(mapProvider) {
+	var This = this;
+	this.mapProvider = mapProvider;
+	this.picsAnalyzer = new PicsAnalyzer();
+	this.mapWindow = new MapWindow();
+	this.loaded = false;
+
+	this.markersReady = function(event, markers) {
+	    if (markers.length == 0) {
+		This.mapWindow.failToLoad();
+		return;
+	    }
+	    var container = This.mapWindow.getMapContainer();
+	    This.mapWindow.hideProgressBar();
+	    This.mapProvider.loadMap(container, markers);
+	}
+	
+	this.openMap = function() {
+	    This.mapWindow.open();
+	    if ( !This.loaded ) {
+		This.loaded = true;
+		This.picsAnalyzer.loadImages();
+	    }
+	}
+	
+	//ctor
+	this.picsAnalyzer.bind('finish', this.markersReady)
+	this.picsAnalyzer.bind('progress', function(event, curr, max){ This.mapWindow.progress(curr,max); })
+    }
+    
+
+    var picsToMap = new PicsToMap( new GoogleMaps() );
+
+    $('.geotagged_pics_open_map').click(picsToMap.openMap);
+
 })
